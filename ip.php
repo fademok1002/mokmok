a3b032a23972677a1af5749cbee1345e9b5f59f3
fademok1002 <fademok100@gmail.com>
2025-05-08 00:31:41 +0300

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,1 +1,19 @@
 
+<?php
+function getVisitorIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // عنوان IP عند الاتصال من شبكة مشتركة
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // عنوان IP عند الاتصال عبر بروكسي
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        // عنوان IP العادي
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$visitorIP = getVisitorIP();
+echo "عنوان IP الزائر هو: " . $visitorIP;
+?>
