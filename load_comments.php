19d4f3de287b6e0ae79eb922a1ca6911baf19e78
fademok1002 <fademok100@gmail.com>
2025-05-08 04:56:56 -0400

    Update load_comments.php

--- load_comments.php
+++ load_comments.php
@@ -1,9 +1,7 @@
-
 <?php
-// التحقق إذا كان الملف موجودًا
-if (file_exists('comments.txt')) {
-    // قراءة محتويات الملف النصي
-    $comments = file_get_contents('comments.txt');
+// قراءة محتوى ملف التعليقات
+if (file_exists("comments.txt")) {
+    $comments = file_get_contents("comments.txt");
     echo $comments;
 } else {
     echo "لا توجد تعليقات بعد.";
