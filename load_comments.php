5480b8002e5fa1660a5cccb27efd64d6055e8b75
fademok1002 <fademok100@gmail.com>
2025-05-08 05:10:39 -0400

    Delete load_comments.php

--- load_comments.php
+++
@@ -1,9 +0,0 @@
-<?php
-// قراءة محتوى ملف التعليقات
-if (file_exists("comments.txt")) {
-    $comments = file_get_contents("comments.txt");
-    echo $comments;
-} else {
-    echo "لا توجد تعليقات بعد.";
-}
-?>
