3ef4eddaa5244deb80d36cb493bc222e86c2fe59
fademok1002 <fademok100@gmail.com>
2025-05-08 04:12:05 -0400

    Update save_comment.php

--- save_comment.php
+++ save_comment.php
@@ -1,1 +1,17 @@
 
+<?php
+// التحقق إذا تم إرسال البيانات عبر POST
+if (isset($_POST['name']) && isset($_POST['comment'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $comment = htmlspecialchars($_POST['comment']);
+
+    // فتح أو إنشاء الملف النصي
+    $file = fopen("comments.txt", "a");
+
+    // إضافة التعليق إلى الملف النصي مع اسم المرسل
+    fwrite($file, "<strong>" . $name . ":</strong><p>" . $comment . "</p><hr>\n");
+
+    // غلق الملف بعد الكتابة
+    fclose($file);
+}
+?>
